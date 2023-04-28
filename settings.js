54b9d972749eef15463a5b3da32f767ac68c785a
nobaraaleviathan <123747315+nobaraaleviathan@users.noreply.github.com>
2023-04-28 15:20:57 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
 global.session = " " //session Id
-global.ownernomer = "3278169630" //ur number
+global.ownernomer = "393278169630" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94786802371'] //ur premium number
 global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
